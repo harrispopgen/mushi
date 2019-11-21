47691c4b722618aef1754a338f441be03809e2a9
William DeWitt <wsdewitt@gmail.com>
2019-11-21 08:27:57 -0800

    1kg update

--- 1KG/qsub_scons.sh
+++ 1KG/qsub_scons.sh
@@ -1,26 +1,28 @@
 #!usr/bin/bash
 
-cd /net/harris/vol1/project/mushi/1KG
-source activate mushi
+# cd /net/harris/vol1/project/mushi/1KG
+# source activate mushi
 
 # linux
-n_jobs=`nproc --all`
+# n_jobs=`nproc --all`
 # n_jobs=1
 # mac
-# n_jobs=`sysctl -n hw.physicalcpu`
+n_jobs=`sysctl -n hw.physicalcpu`
 
-bcfs="/net/harris/vol1/data/phase3_1000genomes/bcfs"
-ancs="/net/harris/vol1/data/phase3_1000genomes/human_ancestor_GRCh37_e59"
-sample_pops="/net/harris/vol1/data/phase3_1000genomes/integrated_call_samples_v3.20130502.ALL.panel"
-k="5"
-mutation_rate="1.1e-8"
-phastcons="/net/harris/vol1/data/phase3_1000genomes/phastConsElements100way.txt.gz"
-repeats="/net/harris/vol1/data/phase3_1000genomes/nestedRepeats.txt.gz"
+bcfs="../data/phase3_1000genomes/bcfs"
+ancs="../data/human_ancestor_GRCh37_e59"
+sample_pops="../data/phase3_1000genomes/integrated_call_samples_v3.20130502.ALL.panel"
+k="3"
+phastcons="../data/phastConsElements100way.txt.gz"
+repeats="../data/nestedRepeats.txt.gz"
 
-outdir="/net/harris/vol1/project/mushi/1KG/scons_output_5mer"
+outdir="scons_output_noenhancers"
+
+track='--track=../data/20161129_enhancer_states.txt.4567.bed'
+comp='--track_complement'
 
 # no_exec="--no-exec"
 
-cmd="scons --bcfs=${bcfs} --ancs=${ancs} --mutation_rate=${mutation_rate} --kmer=${k} --phastcons=${phastcons} --repeats=${repeats} --jobs=${n_jobs} --outdir=${outdir} ${debug} ${no_exec} --sample_pops=${sample_pops} ${gl}"
+cmd="scons ${track} ${comp} --bcfs=${bcfs} --ancs=${ancs} --kmer=${k} --phastcons=${phastcons} --repeats=${repeats} --jobs=${n_jobs} --outdir=${outdir} ${debug} ${no_exec} --sample_pops=${sample_pops} ${gl}"
 echo $cmd
 $cmd
