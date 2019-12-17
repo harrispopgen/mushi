fe4030eed88c7a460ff8674d8d6b49e6549d35e5
William DeWitt <wsdewitt@gmail.com>
2019-12-17 15:54:50 -0800

    remove coord descent, and separate inf for 1KG subpops

--- 1KG/qsub_scons.sh
+++ 1KG/qsub_scons.sh
@@ -11,18 +11,28 @@ n_jobs=`sysctl -n hw.physicalcpu`
 
 bcfs="../data/phase3_1000genomes/bcfs"
 ancs="../data/human_ancestor_GRCh37_e59"
+
 sample_pops="../data/phase3_1000genomes/integrated_call_samples_v3.20130502.ALL.panel"
+
+# # superpopulations
+# pops="AFR,AMR,EAS,EUR,SAS"
+
+# africans
+pops="YRI,LWK,GWD,MSL,ESN,ASW,ACB"
+
 k="3"
+
 phastcons="../data/phastConsElements100way.txt.gz"
 repeats="../data/nestedRepeats.txt.gz"
 
-outdir="scons_output_noenhancers"
+# outdir="scons_output"
+outdir="scons_output_AFR"
 
-track='--track=../data/20161129_enhancer_states.txt.4567.bed'
-comp='--track_complement'
+# track='--track=../data/20161129_enhancer_states.txt.4567.bed'
+# comp='--track_complement'
 
 # no_exec="--no-exec"
 
-cmd="scons ${track} ${comp} --bcfs=${bcfs} --ancs=${ancs} --kmer=${k} --phastcons=${phastcons} --repeats=${repeats} --jobs=${n_jobs} --outdir=${outdir} ${debug} ${no_exec} --sample_pops=${sample_pops} ${gl}"
+cmd="scons ${track} ${comp} --pops=${pops} --bcfs=${bcfs} --ancs=${ancs} --kmer=${k} --phastcons=${phastcons} --repeats=${repeats} --jobs=${n_jobs} --outdir=${outdir} ${debug} ${no_exec} --sample_pops=${sample_pops} ${gl}"
 echo $cmd
 $cmd
