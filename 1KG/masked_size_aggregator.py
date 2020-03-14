d174f7d6b3895eaf9d3e22ee965a1ad1cb4c44a0
William DeWitt <wsdewitt@gmail.com>
2020-03-13 19:13:06 -0700

    masked size aggregator

---
+++ 1KG/masked_size_aggregator.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from collections import Counter
+
+sizes = Counter()
+for f in sys.argv[1:]:
+    for line in open(f):
+        context, count = line.split()
+        sizes[context] += int(count)
+
+for context in sorted(sizes):
+    print(f'{context}\t{sizes[context]}')
