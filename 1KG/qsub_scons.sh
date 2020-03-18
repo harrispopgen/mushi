f0f796e38f7e1d7d4104797af81392cd99a7feb9
William DeWitt <wsdewitt@gmail.com>
2020-03-18 10:37:25 -0700

    tidy scons

--- 1KG/qsub_scons.sh
+++ 1KG/qsub_scons.sh
@@ -9,24 +9,23 @@
 n_jobs=`sysctl -n hw.physicalcpu`
 
 bcfs="../data/phase3_1000genomes/bcfs"
-
 ancs="../data/human_ancestor_GRCh37_e59"
-
 samples="../data/phase3_1000genomes/integrated_call_samples_v3.20130502.ALL.panel"
+mask="../data/phase3_1000genomes/20140520.pilot_mask.autosomes.bed"
 
-# all 1KG
+# restrict to pops
 # pops="--pops=GBR,FIN"
 
+# restrict to one chromosome
+# chrom="--chrom=22"
+
+# kmer
 k="3"
 
-mask="../data/phase3_1000genomes/20140520.strict_mask.autosomes.bed"
-
-mushi_cfg="3mer.cfg"
-
-outdir="scons_output"
+outdir="scons_output_pilot"
 
 # no_exec="--no-exec"
 
-cmd="scons --mask=${mask} ${pops} --bcfs=${bcfs} --ancs=${ancs} --kmer=${k} --jobs=${n_jobs} --outdir=${outdir} ${debug} ${no_exec} --samples=${samples} --mushi_cfg=${mushi_cfg}"
+cmd="scons --mask=${mask} ${pops} ${chrom} --bcfs=${bcfs} --ancs=${ancs} --kmer=${k} --jobs=${n_jobs} --outdir=${outdir} ${debug} ${no_exec} --samples=${samples}"
 echo $cmd
 $cmd
